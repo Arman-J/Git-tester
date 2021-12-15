6bad288b97c0a6d420a08ad8286698d3ac6798d8
Arman Johal <armanjohal328@gmail.com>
2021-12-15 16:27:19 -0500

    um

--- Hello.java
+++ Hello.java
@@ -8,11 +8,22 @@ public class Hello {
     public static void p(int val) {
         System.out.println(val);
     }
-    public static void main(String[] args) {
-        int x = 2;
-        while (x <= 512){
-            p(x);
-            x = x*2;
+    public static void p(boolean val) {
+        System.out.println(val);
+    }
+    public static boolean memberIsLikelyToAttendMeeting(int grade, double meetings) {
+        if(grade >= 9 && grade <= 12) {
+            if(meetings >= 1) {
+                return true;
+            }
+            else { return false; }
         }
+        else { return false; }
+    }
+    public static void main(String[] args) {
+        p(memberIsLikelyToAttendMeeting(3, 4.5));
+        p(memberIsLikelyToAttendMeeting(12, 8.9));//damm how that even possiable
+        p(memberIsLikelyToAttendMeeting(8, 0.0000003));
+        p(memberIsLikelyToAttendMeeting(10, 0.0000003));
     }
 }
